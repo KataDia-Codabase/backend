1ee3a31158140d194d6e3ca91cdc37b0ba0aa8d2
Dias <78405123+diasdigital@users.noreply.github.com>
2025-11-19 00:40:55 +0700

    Mempersiapkan controller untuk pengucapan

--- src/models/index.js
+++ src/models/index.js
@@ -4,12 +4,27 @@ const LessonVocab = require('./lessonVocab');
 const Lesson = require('./lesson');
 const User = require('./user');
 const UserProgress = require('./userProgress');
+const PronunciationSubmission = require('./pronunciationSubmission');
 
 // Associations
 Lesson.hasMany(LessonVocab, { foreignKey: 'lesson_id', as: 'lesson_vocab' });
 LessonVocab.belongsTo(Lesson, { foreignKey: 'lesson_id', as: 'lesson' });
 
 User.hasMany(UserProgress, { foreignKey: 'user_id', as: 'progress' });
+User.hasMany(PronunciationSubmission, {
+    foreignKey: 'user_id',
+    as: 'pronunciations',
+});
+UserProgress.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
+PronunciationSubmission.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
+LessonVocab.hasMany(PronunciationSubmission, {
+    foreignKey: 'lesson_vocab_id',
+    as: 'submissions',
+});
+PronunciationSubmission.belongsTo(LessonVocab, {
+    foreignKey: 'lesson_vocab_id',
+    as: 'lesson_vocab',
+});
 
 module.exports = {
     sequelize,
